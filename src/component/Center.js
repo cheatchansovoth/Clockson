587d57a79b933bcb4cb89feef1e30078ebea9063
cheatchansovoth <cheatchansovoth@gmail.com>
2023-03-18 16:43:52 +1300

    update denpendencies

--- src/component/Center.js
+++ src/component/Center.js
@@ -6,7 +6,13 @@ export const Center = () => {
 
   
     return (
-      <div>
+      <div class="bg-[url('https://static.wixstatic.com/media/84770f_b5b78cd83b6342199b7370a2ba6b9e06.jpg/v1/fill/w_1920,h_601,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/84770f_b5b78cd83b6342199b7370a2ba6b9e06.jpg')] bg-cover bg-center h-[50vh]">
+        <div className='flex text-center justify-center items-center h-[50vh]'>
+          <div className='bg-slate-400 p-[5%] px-[10%] bg-opacity-75'>
+            <h1>FALL & WINTER</h1>
+            <button>Shop Now</button>
+          </div>
+        </div>
       </div>
   )
 }
